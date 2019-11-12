bb2463f1aa9be457c36c0948ae0d48564131d983
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-11-12 18:53:46 +0000

    add modified file to use nv-htm in real nvm

--- make_nv-htm_lib.sh
+++ make_nv-htm_lib.sh
@@ -7,6 +7,7 @@ export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$PMDK_PATH/include
 export LIBRARY_PATH=$LIBRARY_PATH:$PMDK_PATH/lib
 
 if [ ! -e nvhtm-selfcontained/nv-htm/libnh.a ]; then
+    cp -R nvhtm_modification_files/* nvhtm-selfcontained/nv-htm/compile.sh
 	(cd nvhtm-selfcontained/nv-htm; ./compile.sh REDO_TS FORK)
 fi
 
