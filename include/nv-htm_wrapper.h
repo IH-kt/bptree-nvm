2a072615730e782bfd3c33ad93d5c41844aac527
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-11-20 09:00:55 +0000

    re-implement nvhtm

--- include/nv-htm_wrapper.h
+++ include/nv-htm_wrapper.h
@@ -8,7 +8,14 @@
 #    define NVM_read(p) NH_read(p)
 #  else
 #    define NVHTM_begin()
+#    define NVHTM_thr_init()
+#    define NVHTM_thr_exit()
+#    define NVHTM_init(d)
+#    define NVHTM_clear()
+#    define NVHTM_shutdown()
+#    define NVHTM_cpy_to_checkpoint(p)
 #    define NVHTM_end()
+#    define NH_alloc(s) malloc(s)
 #    define NVM_write(p, val) *p = val
 #    define NVM_read(p) *p
 #  endif
